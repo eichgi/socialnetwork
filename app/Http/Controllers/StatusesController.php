b09cd6d7720d69c9bece3e70ae6fdcf4af3a61bc
HG <hiramg90@gmail.com>
2018-08-25 13:24:30 -0500

    Creating tests for validating json responses

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -43,6 +43,9 @@ class StatusesController extends Controller
      */
     public function store(Request $request)
     {
+
+        request()->validate(['body' => 'required|min:5']);
+
         $status = Status::create([
             'body' => request('body'),
             'user_id' => auth()->id()
--- tests/Feature/CreateStatusTest.php
+++ tests/Feature/CreateStatusTest.php
@@ -33,21 +33,49 @@ class CreateStatusTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        //1.- Given => Teniendo un usuario autenticado
         $user = factory(User::class)->create();
         $this->actingAs($user);
 
-        //2.- When => Cuando hace un post request a status
         $response = $this->post(route('statuses.store'), ['body' => 'Mi primer status']);
 
         $response->assertJson([
             'body' => 'Mi primer status'
         ]);
 
-        //3.- Then => Entonces veo un nuevo estado en la base de datos
         $this->assertDatabaseHas('statuses', [
             'user_id' => $user->id,
             'body' => 'Mi primer status'
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function a_status_requires_a_body()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('statuses.store'), ['body' => '']);
+        //dd($response->getContent());
+        //$response->assertSessionHasErrors('body');
+        $response->assertStatus(100);
+        $response->assertJsonStructure([
+            'message',
+            'errors' => ['body']
+        ]);
+    }
+
+    public function test_a_status_body_requires_a_minimum_length()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('statuses.store'), ['body' => 'body']);
+        $response->assertStatus(422);
+        $response->assertJsonStructure([
+            'message',
+            'errors' => ['body']
         ]);
     }
 }
