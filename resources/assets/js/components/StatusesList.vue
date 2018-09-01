6c6141e6a2eb9ef1aa29289d151a7cd6745afb0d
HG <hiramg90@gmail.com>
2018-09-01 09:03:30 -0500

    Completed Lesson 16: Styling statuses list and form

--- resources/assets/js/components/StatusesList.vue
+++ resources/assets/js/components/StatusesList.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
-        <div v-for="status in statuses" v-text="status.body"></div>
+        <div class="card mb-3 border-0 shadow-sm" v-for="status in statuses">
+            <div class="card-body d-flex flex-column">
+                <div class="d-flex align-items-center mb-3">
+                    <img class="rounded mr-3 shadow-sm" width="40px" src="https://aprendible.com/images/default-avatar.jpg">
+                    <div>
+                        <h5 class="mb-1">Jorge García</h5>
+                        <div class="small text-muted">Hace una hora</div>
+                    </div>
+                </div>
+                <p class="card-text text-secondary" v-text="status.body"></p>
+            </div>
+        </div>
     </div>
 </template>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-8 mx-auto">
-                <div class="card border-0 bg-light">
+                <div class="card border-0 bg-light mb-3 shadow-sm">
                     <status-form></status-form>
                 </div>
                 <statuses-list></statuses-list>
